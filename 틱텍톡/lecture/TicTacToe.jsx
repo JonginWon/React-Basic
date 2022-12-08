a2806caa0fef72f3137444bf9abe0158fc4f4250
JonginWon <jonginwon1203@gmail.com>
2022-12-09 00:52:53 +0900

    TicTacToe

---
+++ 틱텍톡/lecture/TicTacToe.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Table from "./Table";
+
+const TicTacToe = () => {
+  return (
+    <>
+      <Table />
+      {winner && <div>{winner}님의 승리</div>}
+    </>
+  );
+};
+
+export default TicTacToe;
