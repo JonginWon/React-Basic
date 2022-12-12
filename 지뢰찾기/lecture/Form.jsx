9e3af392fa9b164ab6db09493f1b402050d687e7
JonginWon <jonginwon1203@gmail.com>
2022-12-12 22:04:08 +0900

    createContext setting

--- 지뢰찾기/lecture/Form.jsx
+++ 지뢰찾기/lecture/Form.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useContext } from "react";
+import { TableContext, START_GAME } from "./MineSearch";
 
 const Form = () => {
   const [row, setRow] = useState();
   const [cell, setCell] = useState();
   const [mine, setMine] = useState();
+  const { dispatch } = useContext(TableContext);
 
   const onChangeRow = useCallback((e) => {
     setRow(e.target.value);
@@ -17,7 +19,9 @@ const Form = () => {
     setMine(e.target.value);
   }, []);
 
-  const onClickBtn = useCallback(() => {}, []);
+  const onClickBtn = useCallback(() => {
+    dispatch({ type: START_GAME, row, cell, mine });
+  }, [row, cell, mine]);
 
   return (
     <div>
