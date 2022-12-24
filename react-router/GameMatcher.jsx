59718f979f3f05abd9482aaff992843da9f0c942
JonginWon <jonginwon1203@gmail.com>
2022-12-24 22:11:15 +0900

    리액트 라우터 마무리

--- react-router/GameMatcher.jsx
+++ react-router/GameMatcher.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import NumberBaseball from "../숫자야구/lecture/NumberBassballClass";
 import RSP from "../가위바위보/lecture/RSPClass";
 import Lotto from "../로또/lecture/LottoClass";
 
 class GameMatcher extends Component {
   render() {
+    let url = new URLSearchParams(this.props.location.search.slice(1));
+    console.log(url.get("hello"));
     if (this.props.match.params.name === "number-baseball") {
       return <NumberBaseball />;
-    } else if (this.props.match.params.name === "RSP") {
+    } else if (this.props.match.params.name === "rock-scissors-paper") {
       return <RSP />;
-    } else if (this.props.match.params.name === "Lotto") {
+    } else if (this.props.match.params.name === "lotto-generator") {
       return <Lotto />;
     }
     return <div>일치하는 게임이 없습니다.</div>;
   }
 }
 
-export default GameMatcher;
+export default withRouter(GameMatcher);
